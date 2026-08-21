b2d9bf2ea99c228c7054d404bcfc14681c7b663d
Adonai Pinheiro <adonaijpinheiro@gmail.com>
2026-08-21 14:27:36 -0300

    Initial Commit

---
+++ iOS_USPMovies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS_USPMovies
+//
+//  Created by Adonai Pinheiro on 21/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS_USPMovies/iOS_USPMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_USPMoviesApp.swift
+//  iOS_USPMovies
+//
+//  Created by Adonai Pinheiro on 21/08/26.
+//
+
+import SwiftUI
+
+@main
+struct iOS_USPMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
